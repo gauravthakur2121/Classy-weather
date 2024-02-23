300eb26acdda619f32ced65e4ce6200a83a699da
gauravthakur2121 <shishodiag12@gmail.com>
2024-02-23 23:14:22 +0530

    start the project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className='app'>
+          <h1> CLASSY WEATHER</h1>
+          <input type='text'></input>
+
+        </div>
+        
+      </div>
+    )
+  }
+}
